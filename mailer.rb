458af6bdf4734e9721d436f18ddf8a26541961b9
bomatson <bobby@carbonfive.com>
2013-11-23 00:00:15 -0800

    use Pony to send email to myself

---
+++ mailer.rb
@@ -0,0 +1,18 @@
+require 'pony'
+
+class Mailer
+  def send_me_email(quote)
+    Pony.mail(to: 'bobby.matson@gmail.com',
+              subject: 'Quote of the Day',
+              body: quote,
+              via: :smtp,
+              via_options: options)
+  end
+
+  private
+
+  def options
+    { address: 'smtp.gmail.com', port: '587', user_name: 'bobby.matson@gmail.com',
+      password: ENV['PASSWORD'], authentication: :login }
+  end
+end
---
+++ spec/mailer_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../mailer'
+require 'rspec'
+
+describe Mailer do
+  before { Mailer.stub(:send_me_email).with 'quoting' }
+
+  subject{ Mailer.send_me_email 'quoting' }
+
+  context '#send_me_mail' do
+    it 'sends me an email' do
+      expect(Mailer).to receive(:send_me_email).with('quoting')
+      subject
+    end
+  end
+end
