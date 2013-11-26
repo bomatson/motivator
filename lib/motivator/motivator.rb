6df66c51ebac9875f0e1e650d3df19c864f0eca2
bomatson <bobby@carbonfive.com>
2013-11-25 23:08:56 -0800

    link up refactored Mailer function to Motivator

--- lib/motivator/motivator.rb
+++ lib/motivator/motivator.rb
@@ -1,12 +1,12 @@
 class Motivator
   include Mailer
 
-  attr_accessor :url
+  attr_accessor :url, :recipients
 
   def encourage!
     crawler = Crawler.new
     quote = crawler.snatch(@url)
-    if Mailer.send_me_email(quote)
+    if Mailer.send_email(quote, @recipients)
       p 'Email sent!'
     else
       p 'Email failed :('
